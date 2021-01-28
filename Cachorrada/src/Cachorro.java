1915609f996916f47a1381cc0c58c0c94f3d2420
Adilson Agottani dos Reis <adilson.reis@pucpr.edu.br>
2021-01-28 20:44:08 -0300

    pequena adição

--- Cachorrada/src/Cachorro.java
+++ Cachorrada/src/Cachorro.java
@@ -1,5 +1,9 @@
-public class App {
+public class Cachorro {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Woof!");
     }
+
+    Pet dog= new Pet();
+    dog.setNome("Kimmy");
+    System.out.println(dog.getNome);
 }
